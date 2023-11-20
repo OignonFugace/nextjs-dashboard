a9c7042d7019308fe7b9ea034abe15bb32c36acf
Tanguy Freycon <tanguy.freycon@gmail.com>
2023-11-20 22:10:24 +0100

    Dynamic render of dashboard page elements

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,6 +14,7 @@ import { unstable_noStore as noStore } from 'next/cache';
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
@@ -34,6 +35,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
 
   try {
     const data = await sql<LatestInvoiceRaw>`
@@ -55,6 +57,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
 
   try {
     // You can probably combine these into a single SQL query
@@ -95,6 +98,7 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  noStore();
 
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
@@ -128,6 +132,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore();
 
   try {
     const count = await sql`SELECT COUNT(*)
@@ -150,6 +155,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
 
   try {
     const data = await sql<InvoiceForm>`
@@ -176,6 +182,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore();
 
   try {
     const data = await sql<CustomerField>`
@@ -195,6 +202,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore();
 
   try {
     const data = await sql<CustomersTable>`
@@ -229,6 +237,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
+  noStore();
 
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
